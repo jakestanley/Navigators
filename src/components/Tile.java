18ed7abe79f5ff1ac92d4a2f33104c6b312dd644
Jake Stanley <prudentia713@gmail.com>
2013-11-14 18:45:27 +0000

    Added solid property to Tile enum

--- src/components/Tile.java
+++ src/components/Tile.java
@@ -4,19 +4,32 @@ import java.awt.Color; // more of trysten's code
 import asciiPanel.AsciiPanel;
 
 public enum Tile {
-    FLOOR((char)177, AsciiPanel.black),
-    VOID((char)250, AsciiPanel.white),
-    WALL((char)177, AsciiPanel.white),
-    DOOR((char)177, AsciiPanel.red);
+	
+    FLOOR((char)177, AsciiPanel.black, false),
+    VOID((char)250, AsciiPanel.white, false),
+    WALL((char)177, AsciiPanel.white, true),
+    DOOR((char)177, AsciiPanel.red, false); // need open and closed door
  
     private char glyph;
-    public char glyph() { return glyph; }
- 
     private Color color;
-    public Color color() { return color; }
- 
-    Tile(char glyph, Color color){
+    private boolean solid;
+    
+    Tile(char glyph, Color color, boolean solid){
         this.glyph = glyph;
         this.color = color;
+        this.solid = solid;
     }
+    
+    public char glyph(){
+    	return glyph;
+    }
+    
+    public Color color(){
+    	return color;
+    }
+    
+    public boolean solid(){
+    	return solid;
+    }
+    
 }
