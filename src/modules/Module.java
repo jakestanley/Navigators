83b43ef0c2170341a2e5ec2cf9489ec3e004f0dd
Jake Stanley <prudentia713@gmail.com>
2013-11-14 00:00:24 +0000

    Added some new notes and properties to module

--- src/modules/Module.java
+++ src/modules/Module.java
@@ -21,10 +21,40 @@ package modules;
  * 
  * I have an idea that also an atmosphere could consist of liquids such as water or magma, but 
  * I'm not sure if this will work in the same percentage kind of a way. I need some science help.
+ * 
+ * I'm not sure if this is going to be a part of modules or a future utilities/tools component, 
+ * but mechanical crew members (i.e robots) will be more proficient with equipment manufactured 
+ * by their manufacturer. Kind of like how Apple products work better with one another. In a 
+ * similar way, I kind of like the idea that some cheaper brands will break easily, will be 
+ * difficult for low proficiency crew members to use, or other problems will arise from them, 
+ * prompting users to consider what they can get for their money. Maybe there could be a reviews 
+ * system??? i will use brands in modules for now. Locking a module should ideally lock all doors 
+ * surrounding the module.
+ * 
+ * All modules have an operating percentage and energy usage factor. At 100% capacity, the module 
+ * will use the exact specified energy usage. This will increase or decrease depending on the 
+ * percentage. The energy usage factor is used as fuel usage for reactors? Current operating %age
+ * can be increased well over 100%, however performance becomes unstable above 100% and prone to 
+ * failure much more quickly. Exploding modules can harm crew members, depressurise the module or 
+ * in some cases destroy entire ships. An exploding reactor pretty much means instant death for 
+ * an entire crew.
+ * 
+ * Room temperature is altered by life support, increased by module operating heat, decreased by vacuum 
+ * exposure (except in star/hot planet vicinity), coolant, water, ice, etc.
+ *
+ * TODO answer these questions: Should modules have coolant? Technical: Is some form of identification 
+ * necessary? Some ships will have more than two of a kind of module. How will modules map to the ship view? 
+ * Ships should be able to be drawn from a ship object. That's going to be a fun function to write :)
  *
  */
 
 public class Module { // i'm unsure... should this be an interface or a class?
+	
+	protected String brand; // will probably use an int later. not sure... TODO
+	protected float currentOperatingPercentage; // anything over 100% will be risky - calculated by subclass.
+	protected float energyUsage; // this is the energy usage at 100% operation
+	protected float roomTemperature; // measured in celsius
+	protected float condition; // max 100%. directly affects performance.
 	
 	// GASES
 	protected float oxygenPercentage; // healthy to humans
